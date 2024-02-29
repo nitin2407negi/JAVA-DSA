7df0cd6d9aa582ce13cd583f6dee0c5f465b78b0
nitin2407negi <nitinnegi241@gmail.com>
2024-02-29 10:50:04 +0530

    maximum repeated charcter

---
+++ Maxrepchar.java
@@ -0,0 +1,23 @@
+class Maxrepchar
+{
+    public static void main(String args[])
+    {
+        String s="nitin negi";
+        int arr[]=new int[127];
+        for(int i=0;i<s.length();i++)
+        {
+            arr[s.charAt(i)]=arr[s.charAt(i)]+1;
+        }
+        int max=-1;
+        char c=' ';
+        for(int i=0;i<s.length();i++)
+        {
+            if(max<arr[s.charAt(i)])
+            {
+                max=arr[s.charAt(i)];
+                c=s.charAt(i);
+            }
+        }
+        System.out.println("maximum time repeated charcter="+c);
+    }
+}
