92cb0f231e8f39e2bd02d53afe9df0bb549c8cb0
nitin2407negi <nitinnegi241@gmail.com>
2024-06-25 10:22:47 +0530

    negative to positive

---
+++ Arrays/Negtopositive.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Negtopositive {
+    public static void main(String args[])
+    {
+        int arr[]={12, 11, -13, -5, 6, -7, 5, -3, -6};
+        Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+
+
+    }
+}
